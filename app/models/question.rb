864e3b61f2e2ef8412f2ede8b599bb27e78002cc
Peter-Fagan <peter_fagan@hotmail.com>
2016-09-19 14:00:30 +1000

    Pete - created seed file

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,8 +6,10 @@
 #  content    :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
 #
 
 class Question < ApplicationRecord
   has_many :answers
+  belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+u1 = User.create! :email => 'john@gmail.com', :password => 'chicken', :password_confirmation => 'chicken'
+u2 = User.create! :email => 'badger@ga.co', :password => 'chicken', :password_confirmation => 'chicken'
+
+p User.all
+
+Question.destroy_all
+
+q1 = Question.create :content => "Is this a question?"
+q2 = Question.create :content => "Do i really care?....really?"
+
+u1.questions << q1
+u1.questions << q2
+
+Answer.destroy_all
+
+a1 = Answer.create :content => "Yes"
+a2 = Answer.create :content => "No"
+a3 = Answer.create :content => "nope"
+a4 = Answer.create :content => "not a chance"
+
+q1.answers << a1 << a2
+q2.answers << a3 << a4
+
+p u1.questions
+p q1.answers
+p u1.q2.answers
