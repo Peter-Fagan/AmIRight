0af1e4b32d29b176dfb26f1fa063fd9cdb27e795
PomHaffs <tomashaffenden@icloud.com>
2016-09-19 10:25:06 +1000

    Tom - added model for Qs and Answer

---
+++ app/models/answer.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: answers
+#
+#  id          :integer          not null, primary key
+#  content     :text
+#  value       :integer
+#  question_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Answer < ApplicationRecord
+  belongs_to :question
+end
